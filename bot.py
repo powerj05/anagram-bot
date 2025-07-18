9a9000d921eb2046a45af7e2fa9dc3937f016e24
John Power <johnpatrickpower2203@gmail.com>
2025-07-18 15:33:51 +0100

    Removed message handler

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import Application, InlineQueryHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import Application, InlineQueryHandler, ContextTypes
 import sys
 import uuid
 
@@ -50,31 +50,9 @@ async def inline_query_handler(update: Update, context: ContextTypes.DEFAULT_TYP
             )
     await update.inline_query.answer(results[:50], cache_time=1)
 
-def handle_response(text: str) -> str:
-    return(f"Thanks. We received {text}.\nwoag. get_anagrams logic update??")
-
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message_type: str = update.message.chat.type
-    text: str = update.message.text
-
-    logging.info(f"User {update.message.chat.id} in {message_type}: {text}")
-
-    if message_type == 'group':
-        if BOT_USERNAME in text:
-            new_text: str = text.replace(BOT_USERNAME, '').strip()
-            response: str = handle_response(new_text)
-        else:
-            return
-    else:
-        response: str = handle_response(text)
-    
-    logging.info(f"Bot: {response}")
-    await update.message.reply_text(response)
-
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
 
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     application.add_handler(InlineQueryHandler(inline_query_handler))
 
     application.run_webhook(
