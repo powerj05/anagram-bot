e7aa25729fa2c285d3788ca653b0913d6370bf19
John Power <johnpatrickpower2203@gmail.com>
2025-07-21 17:25:33 +0100

    Added "one away" functionality, and start and help commands.

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import Application, InlineQueryHandler, ContextTypes
+from telegram.ext import Application, InlineQueryHandler, ContextTypes, CommandHandler
 import sys
 import uuid
 from dotenv import load_dotenv
@@ -37,6 +37,13 @@ def get_anagrams(letters: str):
     
     return sorted(found)
 
+def get_oneaway(word: str):
+    logging.info(f"Getting words one away from {word}")
+    matches = set(w for w in VALID_WORDS if len(w) == len(word) and sum(a!=b for a,b in zip(w,word))==1)
+    logging.info(f"Found {len(matches)} one away from {word}")
+    return matches
+
+
 def get_cross(pattern: str):
     valid_by_length = set(word for word in VALID_WORDS if len(word) == len(pattern))
     pattern = pattern.lower().replace("?",".").replace("_",".")
@@ -47,14 +54,17 @@ def get_cross(pattern: str):
 
 async def inline_query_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.inline_query.query.strip()
+    logging.info(f"Received query {query}")
     results = []
-    if query.endswith(".") and len(query) <= 10:
+    if query.endswith(".") and len(query) <= 20:
         query = query[:-1]
 
         if(query.startswith("anagram ")):
-            words = get_anagrams(query[len: "anagram "])
+            words = get_anagrams(query[len("anagram "):])
         elif(query.startswith("cross ")):
-            words = get_cross(query[len("cross ")])
+            words = get_cross(query[len("cross "):])
+        elif(query.startswith("oneaway ")):
+            words = get_oneaway(query[len("oneaway "):])
         else:
             if("_" in query or "?" in query):
                 words = get_cross(query)
@@ -71,10 +81,25 @@ async def inline_query_handler(update: Update, context: ContextTypes.DEFAULT_TYP
             )
     await update.inline_query.answer(results[:50], cache_time=1)
 
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    help_text = ("How to use this bot:\n\n"
+                 f"'{BOT_USERNAME} anagram aelst.' - All anagrams of letters aelst.\n"
+                 f"'{BOT_USERNAME} cross l_a_t.' - All words matching that pattern of letters and blanks, e.g. least, leapt, etc.\n"
+                 f"'{BOT_USERNAME} oneaway least.' - All words that differ from least by exactly one letter.\n\n"
+                 "Submit query by ending with full stop '.'\n"
+                 )
+    await update.message.reply_text(help_text)
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = "Welcome! Use this bot to generate anagrams, match empty crossword patterns, and more! Use /help to learn more."
+    await update.message.reply_text(text)
+
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
 
     application.add_handler(InlineQueryHandler(inline_query_handler))
+    application.add_handler(CommandHandler("start", start_command))
+    application.add_handler(CommandHandler("help", help_command))
 
     application.run_webhook(
         listen="0.0.0.0",
